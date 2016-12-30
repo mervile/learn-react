c0405eee7be9244e5f8111edc6b4b2b6647d872e
Mervi Leino <mervi.h.leino@gmail.com>
2016-12-30 20:21:50 +0200

    Added some unit tests for actions

--- test/actions.spec.ts
+++ test/actions.spec.ts
@@ -10,6 +10,7 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 const expect = chai.expect;
 
+const todo = { description: 'do something', id: 234, status: 0 };
 let state: ITodosState = {
     didInvalidate: true,
     error: null,
@@ -18,13 +19,12 @@ let state: ITodosState = {
     requestStatus: { isLoading: false, type: '' },
 };
 
-describe('async actions', () => {
+describe('Todos actions', () => {
     afterEach(() => {
         fetchMock.restore();
     });
 
     it('creates RECEIVE_TODOS when fetching todos has been done', () => {
-        const todo = { description: 'do something', id: 234, status: 0 };
         fetchMock.mock('http://localhost:8080/todos', [todo]);
 
         const expectedActions = [
@@ -39,4 +39,84 @@ describe('async actions', () => {
                 expect(store.getActions()).to.eql(expectedActions);
             });
     });
+
+    it('creates REQUEST_TODOS_FAILURE when fetching todos fails', () => {
+        const error = 'DB error';
+        fetchMock.mock('http://localhost:8080/todos', { body: error, status: 500 });
+
+        const expectedActions = [
+            { type: actions.REQUEST_TODOS },
+            { type: actions.REQUEST_TODOS_FAILURE, error },
+        ];
+        const store = mockStore({ todos: state });
+
+        return store.dispatch(actions.getTodosIfNeeded())
+            .then(() => { // return of async actions
+                console.log(JSON.stringify(store.getActions()));
+                const storeActions = store.getActions();
+                expect(storeActions[0]).to.eql(expectedActions[0]);
+                expect(storeActions[1].type).to.eql(actions.REQUEST_TODOS_FAILURE);
+            });
+    });
+});
+
+describe('Todo actions', () => {
+    afterEach(() => {
+        fetchMock.restore();
+    });
+
+    it('creates ADD_TODO_SUCCESS when adding a todo succeeds', () => {
+        fetchMock.post('http://localhost:8080/todo', todo);
+
+        const expectedActions = [
+            { type: actions.ADD_TODO, description: todo.description },
+            { type: actions.ADD_TODO_SUCCESS, todo },
+        ];
+        const store = mockStore({ todos: state });
+
+        return store.dispatch(actions.requestAddTodo(todo.description))
+            .then(() => {
+                const storeActions = store.getActions();
+                expect(storeActions).to.eql(expectedActions);
+            });
+    });
+
+    it('creates ADD_TODO_FAILURE when adding a todo fails', () => {
+        const error = 'DB error';
+        fetchMock.post('http://localhost:8080/todo', { body: error, status: 500 });
+
+        const expectedActions = [
+            { type: actions.ADD_TODO, description: todo.description },
+            { type: actions.ADD_TODO_FAILURE, error },
+        ];
+        const store = mockStore({ todos: state });
+
+        return store.dispatch(actions.requestAddTodo(todo.description))
+            .then(() => {
+                const storeActions = store.getActions();
+                expect(storeActions[0]).to.eql(expectedActions[0]);
+                expect(storeActions[1].type).to.eql(actions.ADD_TODO_FAILURE);
+            });
+    });
+
+    it('creates UPDATE_TODO_SUCCESS when updating a todo succeeds', () => {
+        fetchMock.post('http://localhost:8080/todo', todo);
+
+        const expectedActions = [
+            { type: actions.UPDATE_TODO, todo },
+            { type: actions.UPDATE_TODO_SUCCESS, todo },
+        ];
+        const store = mockStore({ todos: state });
+
+        return store.dispatch(actions.requestUpdateTodo(todo))
+            .then(() => {
+                const storeActions = store.getActions();
+                expect(storeActions).to.eql(expectedActions);
+            });
+    });
+
+    it('creates UPDATE_TODO_FAILURE when updating a todo fails');
+
+    it('creates DELETE_TODO_SUCCESS when deleting a todo succeeds');
+    it('creates DELETE_TODO_FAILURE when deleting a todo fails');
 });
