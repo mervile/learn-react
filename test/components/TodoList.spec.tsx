4a407df50b3ce16c7e808b88ad902b6a7be4eddb
Mervi Leino <mervi.h.leino@gmail.com>
2016-12-30 22:15:56 +0200

    Renamed reducers test and added a unit test for TodoList -component

---
+++ test/components/TodoList.spec.tsx
@@ -0,0 +1,28 @@
+import TodoList from '../../src/components/TodoList';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+const expect = chai.expect;
+
+function setup() {
+    const props = {
+        onDelete: () => {},
+        requestStatus: { isLoading: false, type: '' },
+        todos: [{ description: 'Run tests', id: 0, status: 0 }],
+    };
+
+    const enzymeWrapper = shallow(<TodoList {...props} />);
+
+    return {
+        props,
+        enzymeWrapper,
+    };
+}
+
+describe('TodoList', () => {
+    it('should render self and subcomponents', () => {
+        const { enzymeWrapper } = setup();
+
+        expect(enzymeWrapper.find('ListItem')).to.have.length(1);
+    });
+});
