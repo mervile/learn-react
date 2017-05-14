0eecb63dcc2766fafaae3c783c299268efc995dc
Mervi Leino <mervi.h.leino@gmail.com>
2017-05-14 12:02:04 +0300

    faefs

--- webpack.config.js
+++ webpack.config.js
@@ -85,11 +85,11 @@ var config = {
     ])
   ],
   // Special setup for enzyme
-  externals: {
+ /* externals: {
     'react/addons': true,
     'react/lib/ExecutionEnvironment': true,
     'react/lib/ReactContext': true
-  }
+  }*/
 };
 
 module.exports = config;
